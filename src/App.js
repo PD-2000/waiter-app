7c5a0831451b3d1b3bec63d64d8f5a2056ce4958
Pawel Domagala <domagalapawel11@gmail.com>
2023-05-30 20:29:42 +0200

    Add router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Container } from 'react-bootstrap';
+import { Route } from 'react-router-dom';
+import Home from "./components/pages/Home";
 
 const App = () => {
 	return (
 		<Container>
-			
+			<Route path="/" element={<Home />} />
 		</Container>
 	)
 }

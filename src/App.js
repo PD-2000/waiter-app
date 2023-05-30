e35d13b1eb84955fbc95f0dcc191794425ba18ec
Pawel Domagala <domagalapawel11@gmail.com>
2023-05-30 20:41:46 +0200

    Add NotFound & Table pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Container } from 'react-bootstrap';
 import { Routes, Route } from 'react-router-dom';
 import Home from "./components/pages/Home";
+import Table from "./components/pages/Table";
+import NotFound from "./components/pages/NotFound";
 
 const App = () => {
 	return (
 		<Container>
 			<Routes>
 				<Route path="/" element={<Home />} />
+				<Route path="/table/:id" element={<Table />} />
+				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</Container>
 	)
---
+++ src/components/pages/Table.js
@@ -0,0 +1,9 @@
+const Table = () => {
+	return (
+		<div className="d-flex justify-content-between">
+			<h1>Single table</h1>
+		</div>
+	);
+};
+
+export default Table;
